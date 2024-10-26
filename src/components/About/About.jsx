99304513570f2a435afe58e0baf01b2937bee684
aaditya1904 <adityarajput042005@gmail.com>
2024-10-26 20:17:34 +0530

    resume page left

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import './About.css';
+import "./About.css";
 
 function About() {
   return (
     <>
-    <div className="body">
-
-      <h1>About Me</h1>
-      <p className="content">
-        I'm Aditya Rajput, an undergraduate Computer Science student at Sushila
-        Devi Bansal College of Technology in Indore, Madhya Pradesh. I completed
-        my 10th grade at Mar Thoma H.S. School Indore achieving 63% and 12th
-        grade at Brilliant Academy with 69%. I see challenges as opportunities for growth.
-      </p>
-    </div>
+      <div className="body">
+        <h1>About Me</h1>
+        <p className="content">
+          I’m Aditya Rajput, an undergraduate Computer Science student at
+          Sushila Devi Bansal College of Technology in Indore, Madhya Pradesh. I
+          completed my 10th grade at Mar Thoma H.S. School, achieving 63%, and
+          my 12th grade at Brilliant Academy H.S. School with a score of 69%. I
+          view challenges as valuable opportunities for personal and
+          professional growth, which fuels my passion for learning and
+          development. I have a strong command of HTML, CSS, and Core Java, and
+          I’m building my expertise in React.js. My goal is to combine my
+          technical skills with creative problem-solving to develop innovative
+          solutions. I’m eager to take on new challenges and projects that will
+          allow me to deepen my knowledge and contribute to meaningful work in
+          the tech industry.
+        </p>
+      </div>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import{Link,NavLink} from "react-router-dom"
-import './header.css'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
-   <header>
-    <nav  className='head'> 
-      <ul>
-      <li className='bar-btn' ><NavLink to = '/' style={{color:"white",textDecoration:"none"}} >HOME</NavLink></li>
-      <li className='bar-btn' ><Link to = '/about' style={{color:"white",textDecoration:"none"}}>ABOUT</Link></li>
-      <li className='bar-btn' ><Link to = '/projects' style={{color:"white",textDecoration:"none"}}>PROJECTS</Link></li>
-    </ul>
-    </nav>
-   </header>
-  )
+    <header>
+      <nav className='head'>
+        <ul>
+          <li className='bar-btn'>
+            <NavLink to='/' style={{ textDecoration: 'none' }}>
+              HOME
+            </NavLink>
+          </li>
+          <li className='bar-btn'>
+            <Link to='/about' style={{ textDecoration: 'none' }}>
+              ABOUT
+            </Link>
+          </li>
+          <li className='bar-btn'>
+            <Link to='/projects' style={{ textDecoration: 'none' }}>
+              PROJECTS
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
